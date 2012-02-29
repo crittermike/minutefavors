3442e8f4e9b278a4812467e82571fabaccbb9202
Mike Crittenden <mikecrittenden@gmail.com>
2012-02-29 17:10:15 -0500

    add start of task approval or rejection

--- app/controllers/favors_controller.rb
+++ app/controllers/favors_controller.rb
@@ -31,7 +31,7 @@ class FavorsController < ApplicationController
   # GET /favors/1.json
   def show
     @favor = Favor.find(params[:id])
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :fenced_code_blocks => true, :lax_html_blocks => true)
     unless @favor.description.nil? 
       @favor.description = markdown.render(@favor.description).html_safe
     end
--- app/models/favor.rb
+++ app/models/favor.rb
@@ -3,8 +3,20 @@ class Favor < ActiveRecord::Base
   validates :title, :presence => true
   validates :points, :presence => true
   belongs_to :user
+  before_save :sanitize_html
 
   def as_json(options={})
     super(:include => :tags)
   end
+
+  include ActionView::Helpers::SanitizeHelper  
+  def sanitize_html # Automatically strips any tags from any string to text typed column  
+    for column in Favor.content_columns  
+      if column.type == :string || column.type == :text # if the column is text-typed  
+        if !self[column.name].nil? # strip html from string if it's not empty  
+          self[column.name] = sanitize(self[column.name])  
+        end  
+      end  
+    end  
+  end   
 end

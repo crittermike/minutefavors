253a3b9beeb17f59c6be485b6ae328246896dc02
Mike Crittenden <mikecrittenden@gmail.com>
2012-02-29 09:42:33 -0500

    add points to users

---
+++ db/migrate/20120229143907_add_points_to_users.rb
@@ -0,0 +1,6 @@
+class AddPointsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :points, :integer
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120224165634) do
+ActiveRecord::Schema.define(:version => 20120229143907) do
 
   create_table "favors", :force => true do |t|
     t.string   "title"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(:version => 20120224165634) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.integer  "points"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
